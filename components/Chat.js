98864dc7c4b965084618ec0b7e7d97268bef7543
Brocal82 <antonioruizbrocal@gmail.com>
2023-09-06 11:14:00 +0200

    Enhancements to React Native Chat Component with Firebase Integration
    This update to the React Native Chat component introduces Firebase integration for real-time message storage and synchronization. Additionally, improvements have been made to the code structure and message management, ensuring an efficient and scalable chat experience with support for multiple users.

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { collection, query, orderBy, onSnapshot, addDoc } from 'firebase/firestore'; // Import Firestore functions
 
-const Chat = ({ route, navigation }) => {
-  const { name, color } = route.params;  //Brings name and bg color selected to Chat
+const Chat = ({ route, navigation, db }) => {
+  const { name, color } = route.params;  // Brings name and bg color selected to Chat
   const [messages, setMessages] = useState([]);
 
   const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    addDoc(collection(db, "messages"), newMessages[0])
   }
 
   const renderBubble = (props) => {
@@ -37,7 +38,7 @@ const Chat = ({ route, navigation }) => {
         user: {
           _id: 2,
           name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
+          avatar: 'https://picsum.photos/140'
         },
       },
       {
@@ -46,9 +47,27 @@ const Chat = ({ route, navigation }) => {
         createdAt: new Date(),
         system: true,
       },
-    
     ]);
   }, []);
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+    const unsubMessages = onSnapshot(q, (docs) => {
+      let newMessages = [];
+      docs.forEach(doc => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis())
+        })
+      })
+      setMessages(newMessages);
+    })
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
+   }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: color }}>
@@ -57,17 +76,16 @@ const Chat = ({ route, navigation }) => {
         renderBubble={renderBubble}
         onSend={messages => onSend(messages)}
         user={{
-          _id: 1
+          _id: route.params.userId, // Extract the user ID from route.params
+          name: route.params.name,   // Extract the name from route.params
         }}
       />
-    {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-    {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior="padding" /> : null }
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+      {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior="padding" /> : null }
     </View>
   );
-  
 }
 
-    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -77,7 +95,3 @@ const styles = StyleSheet.create({
 });
 
 export default Chat;
-    
-    
-    
-    
