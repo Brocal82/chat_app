45379c9d433b6f7b06045137293683fbc1d5f446
Brocal82 <antonioruizbrocal@gmail.com>
2023-09-06 11:10:25 +0200

    Firebase Setup and Navigation
    This commit configures Firebase using the provided firebaseConfig, establishes a connection to Cloud Firestore with getFirestore, and sets up app navigation using React Navigation with a 'Chat' screen connected to the Chat component, passing the Firestore database reference as db for chat functionality.

--- App.js
+++ App.js
@@ -4,11 +4,29 @@ import Start from './components/Start';
 import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyArf4_A5e5sAk1AK9Gj-ODYfaE7VMsdaNs",
+    authDomain: "chat-app-50f52.firebaseapp.com",
+    projectId: "chat-app-50f52",
+    storageBucket: "chat-app-50f52.appspot.com",
+    messagingSenderId: "466786179011",
+    appId: "1:466786179011:web:eb877bb23599097dd34f76",
+    measurementId: "G-CF2GNFJSB0"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -19,9 +37,9 @@ const App = () => {
           component={Start}
         />
         <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+          name="Chat">
+          {(props) => <Chat {...props} db={db} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
