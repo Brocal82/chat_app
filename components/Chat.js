ca539b564bafd2c2cefaa4b39df7cec89b7f311e
Brocal82 <antonioruizbrocal@gmail.com>
2023-09-07 12:04:25 +0200

    Network and Cache Enhancements with InputToolbar
    If applied, this commit introduces improvements in network handling, message caching, and adds the InputToolbar feature to the chat application. It enhances network management by displaying the InputToolbar based on connectivity status and implements message caching for offline access. These changes improve user experience by better handling network issues, preserving chat history, and adding the convenient InputToolbar for users.

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { collection, query, orderBy, onSnapshot, addDoc } from 'firebase/firestore'; // Import Firestore functions
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const { name, color } = route.params;  // Brings name and bg color selected to Chat
   const [messages, setMessages] = useState([]);
+
+  let unsubMessages;
+
+  const loadCachedMessages = async () => {
+    const cachedMessages = await AsyncStorage.getItem('messages') || '[]';
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  const cacheMessages = async (messages) => {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(messages));
+    } catch (error) {
+      console.error('Error caching messages:', error);
+    }
+  };
 
   const onSend = (newMessages) => {
     addDoc(collection(db, "messages"), newMessages[0])
@@ -24,6 +40,12 @@ const Chat = ({ route, navigation, db }) => {
       }}
     />
   }
+
+  const renderInputToolbar = (props) => {
+    if (isConnected) 
+    return <InputToolbar {...props} />;
+    else return null;
+   }
 
   useEffect(() => {
     navigation.setOptions({ title: name });
@@ -51,32 +73,46 @@ const Chat = ({ route, navigation, db }) => {
   }, []);
 
   useEffect(() => {
-    navigation.setOptions({ title: name });
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (docs) => {
-      let newMessages = [];
-      docs.forEach(doc => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis())
-        })
-      })
-      setMessages(newMessages);
-    })
-    return () => {
-      if (unsubMessages) unsubMessages();
+    if (isConnected === true) {
+      if (unsubMessages) {
+        unsubMessages();
+      }
+      unsubMessages = null;
+
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q, (querySnapshot) => {
+        let newMessages = [];
+        querySnapshot.forEach((doc) => {
+          newMessages.push({
+            _id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis())
+          });
+        });
+        setMessages(newMessages);
+        cacheMessages(newMessages); // Cache the new messages
+      });
+    } else {
+      loadCachedMessages();
     }
-   }, []);
+
+    return () => {
+      if (unsubMessages) {
+        unsubMessages();
+      }
+    };
+  }, [db, isConnected]);
 
   return (
     <View style={{ flex: 1, backgroundColor: color }}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={messages => onSend(messages)}
         user={{
           _id: route.params.userId, // Extract the user ID from route.params
+          avatar: 'https://picsum.photos/140',
           name: route.params.name,   // Extract the name from route.params
         }}
       />
